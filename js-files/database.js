11f04c13c14be619d6d00c6504663849a811627b
lucky-creations <luckycreations.crochet@gmail.com>
2026-08-03 06:44:30 +0530

    Update database.js

--- js-files/database.js
+++ js-files/database.js
@@ -22,7 +22,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Detects current page
     const path = window.location.pathname;
 
-    if (path.endsWith("index.html") || path === "/complete-catalogue/") {
+    if (path.endsWith("index.html") || path.endsWith("/complete-catalogue/")) {
       logEvent(analytics, "site_visit", { source: "home_page" });
       runTransaction(ref(db, "crochet_stats/crochet_visits"), n => (n || 0) + 1);
     }
